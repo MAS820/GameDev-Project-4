7718a98aa414e855ace2a6f74d14f0cecb9541e8
Mark Stein <markstein820@gmail.com>
2015-11-28 13:04:42 -0500

    Added a GameState GameObject that will control all higher level game functions.
    Made a GameState script component for the GameState that has a count down timer and displays
    it on screen.
    NOTE: Ignore the EventSystem GameObject for now, it needs to be there for the HUD. Also all
    HUD elements need to be a child of Canvas

---
+++ GameDev Project 4/Assets/Scripts/GameState.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameState : MonoBehaviour {
+
+	private float TimeRemaining;
+	public Text countDown;
+
+	// Use this for initialization
+	void Start () {
+		TimeRemaining = 100.0f;
+		countDown.text = TimeRemaining.ToString ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		TimeRemaining -= Time.deltaTime; 
+		countDown.text = ((int)TimeRemaining).ToString ();
+	}
+}
