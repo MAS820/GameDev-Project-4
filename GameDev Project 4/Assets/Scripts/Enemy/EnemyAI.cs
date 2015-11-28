d35f090e82cb73f0e2b67f06a092ac32dc82471e
tfang7 <fangt@rpi.edu>
2015-11-28 14:33:11 -0500

    basic patrolling working, waypoint system added. TODO: cyclops interaction with torch, fix FoV detection script

---
+++ GameDev Project 4/Assets/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+    //Editables //some variables will be used later for animation and navigational mesh
+    public float patrolSpeed = 2f;
+    public float chaseSpeed = 5f;
+    public float chaseWaitTime = 5f;
+    public float patrolWaitTime = 1f;
+
+    public Vector3[] patrolWayPoints;
+    public GameObject[] wayPoints;
+
+    public Vector3 waypoint;
+    private EnemySight enemySight;
+    private NavMeshAgent nav;
+    private GameObject player;
+    private GameObject enemy;
+
+    private int wayPointIndex;
+	// Use this for initialization
+	void Start () {
+        //Field of View Script / Player Detection
+        enemySight = GetComponent<EnemySight>();
+        
+        //Nav Mesh agent sets up the mesh that our characters can move on.
+        nav = GetComponent<NavMeshAgent>();
+        
+        //autobraking causes the character to pause before each waypoint
+        nav.autoBraking = false;
+        
+        //char references
+        player = GameObject.Find("Player");
+        enemy = GameObject.Find("Enemy");
+
+        //Set the first waypoint
+        wayPointIndex = 0;
+
+        //Get the list of waypoint objects.
+        wayPoints = GameObject.FindGameObjectsWithTag("Waypoint");
+
+        
+    }
+	
+	// Update is called once per frame
+    void Patrolling()
+    {
+        //Get the position of the first waypoint
+        waypoint = wayPoints[wayPointIndex].transform.position;
+
+        //Point the enemy at that waypoint
+        transform.LookAt(waypoint);
+
+        //Move enemy towards the point
+        transform.position += transform.TransformDirection(Vector3.forward) * patrolSpeed * Time.deltaTime;
+        Debug.Log(wayPointIndex);
+
+        //IF enemy is close to point, begin to move to next point
+        float distance = Vector3.Distance(waypoint, enemy.transform.position);
+        if (distance <= 3)
+        {
+            //If we reach the end of the list, start over
+            if (wayPointIndex == wayPoints.Length - 1)
+            {
+                wayPointIndex = 0;
+            }
+            //Otherwise, we go through the list.
+            else {
+                wayPointIndex++;
+            }
+        }
+    }
+   
+    void Update()
+    {
+        Patrolling();
+
+    }
+
+}
+
---
+++ GameDev Project 4/Assets/Scripts/Enemy/EnemySight.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+//Still working on this one, will clean up.
+public class EnemySight : MonoBehaviour {
+    public float fieldOfViewAngle = 110f;
+    public bool playerInSight;
+    public Vector3 playerLastSeen;
+    public bool gameOver;
+
+    Light currentLight;
+    private NavMeshAgent nav;
+    private CapsuleCollider col;
+    private Animator anim;
+    private GameObject player;
+    private GameObject enemy;
+    private Animator playerAnim;
+    private Vector3 previousSighting;
+    
+
+    // Use this for initialization
+    void Start () {
+        gameOver = false;
+        //nav = GetComponent<NavMeshAgent>();
+        col = GetComponent<CapsuleCollider>();
+        anim = GetComponent<Animator>();
+        player = GameObject.Find("Player");
+        enemy = GameObject.Find("Enemy");
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!gameOver)
+        {
+        }
+        	
+	}
+    void OnTriggerStay (Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInSight = false;
+            Vector3 direction = other.transform.position - transform.position;
+            float angle = Vector3.Angle(direction, transform.forward);
+            if (angle < fieldOfViewAngle * 0.5f)
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position + transform.up, direction.normalized, out hit, col.radius))
+                {
+                    if (hit.collider.gameObject == player)
+                    {
+                        //player is seen
+                        playerInSight = true;
+                       // enemy.transform.position = player.transform.position;
+                        previousSighting = player.transform.position;
+                    }
+                }
+            }
+        }
+       /* else if (other.gameObject.CompareTag("Lamps") == true )
+        {
+            other.gameObject.GetComponent<Light>().enabled = false;
+
+        }*/
+    }
+    void OnTriggerExit (Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInSight = false;
+        }
+    }
+}
